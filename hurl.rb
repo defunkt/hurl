5af245464c5cc3cf8588bf8ea288078cfdc46978
Chris Wanstrath <chris@ozmm.org>
2009-08-21 19:21:01 -0700

    redis test

--- hurl.rb
+++ hurl.rb
@@ -1,7 +1,12 @@
 require 'sinatra/base'
 
 class Hurl < Sinatra::Base
+  def initialize(*args)
+    super
+    @redis = Redis.new(:host => '127.0.0.1', :port => 6379)
+  end
+
   get '/' do
-    "hi world!"
+    "hi world! #{@redis}"
   end
 end
