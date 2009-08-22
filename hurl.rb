e1de37357c3a3420d04ef6e95c5f699b245c992d
Chris Wanstrath <chris@ozmm.org>
2009-08-21 19:52:25 -0700

    vendor redis

--- hurl.rb
+++ hurl.rb
@@ -1,23 +1,18 @@
 require 'sinatra/base'
 
-begin
-  require 'redis'
-rescue LoadError
-  nil
-end
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/vendor/redis-rb'
+require 'redis'
 
 class Hurl < Sinatra::Base
   dir = File.dirname(File.expand_path(__FILE__))
 
-  set :views, "#{dir}/views"
+  set :views,  "#{dir}/views"
   set :public, "#{dir}/public"
   set :static, true
 
   def initialize(*args)
     super
-    if defined? Redis
-      @redis = Redis.new(:host => '127.0.0.1', :port => 6379)
-    end
+    @redis = Redis.new(:host => '127.0.0.1', :port => 6379)
   end
 
   get '/' do
