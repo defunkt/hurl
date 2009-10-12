04365f2991e6fb391d50e28c22b454d199a6ab2b
Chris Wanstrath <chris@ozmm.org>
2009-10-12 12:25:22 -0700

    nicer syntax for #rubygem

--- libraries.rb
+++ libraries.rb
@@ -5,14 +5,15 @@ require 'base64'
 require 'digest'
 require 'zlib'
 
-def rubygem(file, gem = nil)
+def rubygem(file)
+  gem, file = file.values[0], file.keys[0] if file.respond_to? :keys
   require file
 rescue LoadError
   raise "** Please `gem install #{gem || file.split('/')[0]}`"
 end
 
 rubygem 'sinatra/base'
-rubygem 'yajl', 'yajl-ruby'
+rubygem 'yajl' => 'yajl-ruby'
 rubygem 'curb'
 rubygem 'mustache/sinatra'
 
