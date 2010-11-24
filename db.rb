71b457e6d444cc614e15215a7b4c2ac987619c28
Tianyi Cui <tianyicui@gmail.com>
2010-11-24 20:20:36 +0800

    Ruby 1.9 compatibility
    Address two 1.9.x compatability issues:
    * 1.9.2 doesn't include "." in $LOAD_PATH by default
    * The behavior of mkdir_p on 1.8.7 and 1.9.x differs

--- db.rb
+++ db.rb
@@ -19,7 +19,15 @@ module Hurl
     end
 
     def self.dir(scope, id)
-      FileUtils.mkdir_p "#{DIR}/#{scope}/#{id[0...2]}/#{id[2...4]}/"
+      path = FileUtils.mkdir_p "#{DIR}/#{scope}/#{id[0...2]}/#{id[2...4]}/"
+
+      # In Ruby 1.9, mkdir_p always returns Array,
+      # while in 1.8 it returns String when it has only one item to return.
+      if path.is_a? Array
+        path[0]
+      else
+        path
+      end
     end
 
   private
