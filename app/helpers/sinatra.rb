55f33a75fbea598fdcd6d65e4657724200bf502c
Chris Wanstrath <chris@ozmm.org>
2011-03-19 18:18:57 -0700

    no more shelling out

--- app/helpers/sinatra.rb
+++ app/helpers/sinatra.rb
@@ -14,19 +14,6 @@ module Hurl
         tokens = CodeRay.scan(hash.values.first, hash.keys.first)
         tokens.html.div.sub('CodeRay', 'highlight')
       end
-
-      # shell "cat", :stdin => "file.rb"
-      def shell(cmd, options = {})
-        ret = ''
-        Open3.popen3(cmd) do |stdin, stdout, stderr|
-          if options[:stdin]
-            stdin.puts options[:stdin].to_s
-            stdin.close
-          end
-          ret = stdout.read.strip
-        end
-        ret
-      end
     end
   end
 end
