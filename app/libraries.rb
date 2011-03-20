6c064a9daaad2d4be3767ef02e87028249a521ca
Chris Wanstrath <chris@ozmm.org>
2011-03-19 18:13:01 -0700

    remove albino

--- app/libraries.rb
+++ app/libraries.rb
@@ -15,7 +15,6 @@ require 'yajl'
 require 'curb'
 require 'mustache/sinatra'
 require 'sinatra/auth/github'
-require 'albino'
 require 'coderay'
 
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
